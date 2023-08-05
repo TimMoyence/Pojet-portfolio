0b4066978c6b60c34eb6e68846744d69767d3cec
Tim Moyence <tim.moyence@gmail.com>
2023-08-05 14:27:26 +0200

    fixed the app

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const router = require('./app/router.js');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'app/views'));
 
-app.use(express.static('./public'));
+app.use(express.static('/public'));
 
 app.use(router);
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.set('base_url', process.env.BASE_URL + ':' + app.get('port'));
 
 app.listen(app.get('port'), () => {
