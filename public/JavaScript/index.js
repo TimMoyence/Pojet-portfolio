91c807668ff8729d1fc9b2767b522da3d31f6e14
Tim Moyence <tim.moyence@gmail.com>
2023-09-03 15:08:40 +0200

    typewriter

--- public/JavaScript/index.js
+++ public/JavaScript/index.js
@@ -7,11 +7,29 @@ KnowMoreClicked.addEventListener("click", function () {
 
 const txtAnim = document.querySelector(".title-dev");
 
+let typewriter = new Typewriter(txtAnim, {
+  loop: true,
+  deleteSpeed : 20,
+});
 
-new Typewriter(txtAnim, {
-  deleteSpeed: 20,
-})
-  .TypeString("Developpeur Web full stack,")
+typewriter
+  .typeString("Developpeur Web full stack, ")
   .pauseFor(300)
-  .typeString("Specialisation back end")
+  .typeString("<strong>back end</strong>")
+  .pauseFor(300)
+  .deleteChars(8)
+  .typeString('<span style="color: #27ae60;">JavaScript</span>')
+  .pauseFor(100)
+  .deleteChars(10)
+  .typeString('<span style="color: red;">PostgresSQL</span>')
+  .pauseFor(100)
+  .deleteChars(11)
+  .typeString('<span style="color: green;">Sportif</span>')
+  .pauseFor(100)
+  .deleteChars(7)
+  .typeString('<span style="color: yellow;">Motivé</span>')
+  .pauseFor(100)
+  .deleteChars(7)
+  .typeString('<span style="color: blue;">Passionné</span>')
+  .pauseFor(100)
   .start();
