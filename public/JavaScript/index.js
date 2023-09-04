f36e260fef4c693f09aa6918d83d9de192714106
Tim Moyence <tim.moyence@gmail.com>
2023-09-04 15:47:40 +0200

    fixed image carrousel and button

--- public/JavaScript/index.js
+++ public/JavaScript/index.js
@@ -9,7 +9,8 @@ const txtAnim = document.querySelector(".title-dev");
 
 let typewriter = new Typewriter(txtAnim, {
   loop: true,
-  deleteSpeed : 20,
+  deleteSpeed: 15,
+  delay: 80,
 });
 
 typewriter
@@ -18,18 +19,18 @@ typewriter
   .typeString("<strong>back end</strong>")
   .pauseFor(300)
   .deleteChars(8)
-  .typeString('<span style="color: #27ae60;">JavaScript</span>')
+  .typeString('<span style="color: #E19641;">JavaScript</span>')
   .pauseFor(100)
   .deleteChars(10)
-  .typeString('<span style="color: red;">PostgresSQL</span>')
+  .typeString('<span style="color: #275D84;">PostgreSQL</span>')
   .pauseFor(100)
-  .deleteChars(11)
-  .typeString('<span style="color: green;">Sportif</span>')
-  .pauseFor(100)
-  .deleteChars(7)
-  .typeString('<span style="color: yellow;">Motivé</span>')
+  .deleteChars(10)
+  .typeString('<span style="color: #28A050;">Sportif</span>')
   .pauseFor(100)
   .deleteChars(7)
-  .typeString('<span style="color: blue;">Passionné</span>')
+  .typeString('<span style="color: #4178F0;">Motivé</span>')
+  .pauseFor(100)
+  .deleteChars(7)
+  .typeString('<span style="color: #C89696;">Passionné</span>')
   .pauseFor(100)
   .start();
